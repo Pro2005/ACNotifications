2100f9b0eeececd83c8bd8b854eb38827414d4c4
Avtolic <Yury@Dankes-Air.fritz.box>
2016-07-20 14:33:35 +0200

    Base GUI for demo

---
+++ ACNotificationsDemo/ACNotificationsDemo/DemoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DemoViewController.swift
+//  ACNotificationsDemo
+//
+//  Created by Yury on 20/07/16.
+//  Copyright © 2016 Avtolic. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
