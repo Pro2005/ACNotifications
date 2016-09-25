6be5400483c353f5f2155fbcc901fd3d2bfcfc4e
Avtolic <Yury@Dankes-MacBook-Air.local>
2016-09-25 16:10:29 +0200

    DemoViewController GUI changes

--- ACNotificationsDemo/ACNotificationsDemo/DemoViewController.swift
+++ ACNotificationsDemo/ACNotificationsDemo/DemoViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class DemoViewController: UIViewController {
 
+    @IBOutlet var demoView: UILabel!
+    @IBOutlet var demoButton: UIButton!
+    
+    @IBOutlet var animationsTable: UITableView!
+    @IBOutlet var notificationsTable: UITableView!
+    @IBOutlet var presentersTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func onDemo() {
+    }
+    @IBAction func onAnimations() {
+    }
+    @IBAction func onNotifications() {
+    }
+    @IBAction func onPresenters() {
+    }
 }
